d096f01c263eefe751b57b5d27a689138e616898
HEROHEROPA <61680482+HEROHEROPA@users.noreply.github.com>
2022-05-01 13:36:21 +0900

    ルーティング記述変更とそれに伴い変更された生成パスにリンク先を指定

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root to: 'prototypes#index'
   resources :prototypes, only: [:new, :create,:show,:edit,:update,:destroy] do
      resources :comments, only: [:create]
-  resources :users, only: [:show]   
   end   #別モデルのidをデータベースから取得するのではなくパスに送ってコントローラー内で値を取得するためにルーティングのネストが存在する。
+  resources :users, only: [:show]   
   # resources :rooms, only: [:new, :create]
 end
